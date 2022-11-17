b6d0d7a7bbb8d75575b6d1dbd31415f30209f11b
thisisthelife0304 <stevenhunt0304@gmail.com>
2022-11-17 05:58:59 +0100

    Layout Completed

--- src/renderer/src/components/DataTable.vue
+++ src/renderer/src/components/DataTable.vue
@@ -5,6 +5,27 @@ import DataService from '../service/DataService'
 
 const record = ref([])
 const dataService = ref<DataService>(new DataService())
+const keyword = ''
+const selectedCategories = ref()
+const categories = ref([
+  { name: 'New York', code: 'NY' },
+  { name: 'Rome', code: 'RM' },
+  { name: 'London', code: 'LDN' },
+  { name: 'Istanbul', code: 'IST' },
+  { name: 'Paris', code: 'PRS' }
+])
+
+const onCategorySelect = () => {
+  console.log('selectedCategories', selectedCategories)
+}
+
+const onTextChanged = () => {
+  console.log('keyword', keyword)
+}
+
+const onPaginate = () => {
+  console.log('onPaginate')
+}
 
 onMounted(() => {
   dataService.value?.getAll().then((res) => {
@@ -14,17 +35,40 @@ onMounted(() => {
 })
 </script>
 <template>
-  <h1>Hello World</h1>
-  <h2>Hello World!!</h2>
-  <Button label="Submit"></Button>
-  <div>
-    <DataTable :value="record" responsive-layout="scroll">
-      <Column field="operator.content" header="Operator"></Column>
-      <Column field="ordz.content" header="OZL"></Column>
-      <Column field="category.content" header="Category"></Column>
-      <Column field="description.content" header="Description"></Column>
-      <Column field="site.content" header="Designation"></Column>
-      <Column field="next_inspection.content" header="n.Pruf"></Column>
-    </DataTable>
-  </div>
+  <Card>
+    <template #title> Magnetinduktive Seilinspektion </template>
+    <template #subtitle> Vue3 + PrimeVue + Electron </template>
+    <template #content>
+      <div class="mb-3 flex">
+        <MultiSelect
+          id="category"
+          v-model="selectedCategories"
+          :options="categories"
+          option-label="name"
+          placeholder="Select Categories"
+          @change="onCategorySelect"
+        />
+        <span class="p-input-icon-left ml-auto">
+          <i class="pi pi-search"></i>
+          <InputText v-model="keyword" type="text" placeholder="Search" @change="onTextChanged" />
+        </span>
+      </div>
+      <div>
+        <DataTable :value="record" responsive-layout="scroll">
+          <Column field="operator.content" header="Operator"></Column>
+          <Column field="ordz.content" header="OZL"></Column>
+          <Column field="category.content" header="Category"></Column>
+          <Column field="description.content" header="Description"></Column>
+          <Column field="site.content" header="Designation"></Column>
+          <Column field="next_inspection.content" header="n.Pruf"></Column>
+        </DataTable>
+        <Paginator
+          :rows="10"
+          :total-records="100"
+          :rows-per-page-options="[10, 20, 30]"
+          @page="onPaginate"
+        ></Paginator>
+      </div>
+    </template>
+  </Card>
 </template>
